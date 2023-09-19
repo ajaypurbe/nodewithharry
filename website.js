00838b5bb3ae1966ca5ff7a1d023254e8e9a1883
ajaypurbe <71184437+ajaypurbe@users.noreply.github.com>
2023-09-19 15:17:22 +0545

    rendering html file

--- website.js
+++ website.js
@@ -1,4 +1,7 @@
 import http from 'http';
+
+import fs from 'fs';
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
@@ -13,7 +16,12 @@ const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.end('<h1> This is About Page</h1><p>this is discriptions for about page</p>');
     }
-    else{
+    else if (req.url == '/hello') {
+        const data = fs.readFileSync('index.html')
+        res.statusCode = 200;
+        res.end(data.toString());
+    }
+    else {
         res.statusCode = 404;
         res.end('<h1> Page Not Found</h1><p>write the correct url to get information</p>')
     }
