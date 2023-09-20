582a291e8c2d112d53c85c638696b55eea17e2fd
ajaypurbe <71184437+ajaypurbe@users.noreply.github.com>
2023-09-20 14:17:03 +0545

    setting up express in project

---
+++ expressapp.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+// Rest of your Express.js code...
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
